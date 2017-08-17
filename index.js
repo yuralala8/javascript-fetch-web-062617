6298cf0f31bc0adfb50d0f496334808a54eaec94
Yura Choi <yc1435@nyu.edu>
2017-08-17 14:41:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+
+const token = '66c6d964c74039c4d094c4189add8e3a0d85b678'
+fetch('https://api.github.com/user/repos', {
+	headers: {
+		Authorization: `token ${token}`
+	}
+}).then(res => res.json()).then(json => console.log(json));
